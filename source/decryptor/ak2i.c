f0cf0242ae57cfc83e80fcb7566d9b8c7614b2af
Sunguk Lee <d3m3vilurr@gmail.com>
2016-12-21 00:11:19 +0900

    Improve safety for inject ap2i bootrom

--- source/decryptor/ak2i.c
+++ source/decryptor/ak2i.c
@@ -133,6 +133,17 @@ u32 InjectAk2iCart(u32 param)
     uint8_t* buffer = BUFFER_ADDRESS + writeLength;
     memset(buffer, 0x41, writeLength);
 
+    Debug("Load patch");
+
+    if (!DebugFileOpen("ak2i_flash81_ntrcardhax.bin")) {
+        return 1;
+    }
+    if (!DebugFileRead(buffer, 0x20000, 0)) {
+        FileClose();
+        return 1;
+    };
+    FileClose();
+
     AK2I_CmdSetMapTableAddress(AK2I_MTN_NOR_OFFSET, 0);
 
     // setup flash to 1681
@@ -163,72 +174,65 @@ u32 InjectAk2iCart(u32 param)
     }
     //ioAK2EraseFlash( 0, CHIP_ERASE );
 
-    Debug("Load patch");
+    //buffer[0x200C] = 'B';
 
-    if (DebugFileCreate("ak2i_flash81_ntrcardhax.bin", false)) {
-        DebugFileRead(buffer, 0x20000, 0);
-        FileClose();
+    Debug("Writing...");
 
-        //buffer[0x200C] = 'B';
-
-        Debug("Writing...");
-
-        for( u32 i = 0; i < writeLength; i += 512 ) {
-            if (hwVer == 0x81) {
-                AK2I_CmdWriteFlash_81(writeBegin + i, buffer + i, 512);
-            } else {
-                AK2I_CmdWriteFlash_44(writeBegin + i, buffer + i, 512);
-            }
-
-            if (!AK2I_CmdVerifyFlash(buffer + i, writeBegin + i, 512)) {
-                Debug("verify failed at %08X", i);
-            }
-            Debug("%08X", i);
+    for( u32 i = 0; i < writeLength; i += 512 ) {
+        if (hwVer == 0x81) {
+            AK2I_CmdWriteFlash_81(writeBegin + i, buffer + i, 512);
+        } else {
+            AK2I_CmdWriteFlash_44(writeBegin + i, buffer + i, 512);
         }
 
-        Debug("Lock flash");
-        AK2i_CmdLockFlash();
-
-        AK2I_CmdSetMapTableAddress(AK2I_MTN_NOR_OFFSET, 0);
-
-        //u32 acek_sign = 0x4B454341; // "ACEK"
-        //u32 ak2k_sign = 0x4B324B41; // "AK2K"
-
-        //ioAK2WriteFlash( NOR_FAT2_START, &ak2k_sign, 4 );
-
-        //end writting
-
-        uint8_t* mem = BUFFER_ADDRESS + 0x10000;
-        memset(mem, 0x41, 0x10000);
-
-        u32 length = hwVer == 0x81 ? 0x1000000 : 0x200000;
-        Debug("Dump patched flash... (length 0x%08X)", length);
-
-        char *filename = "ak2i_flash_patched.bin";
-        DebugFileCreate(filename, true);
-        uint8_t *tmp = mem;
-        for( u32 i = 0, offset = 0, read = 0; i < length; i += 512 ) {
-            ShowProgress(i, length);
-            u32 toRead = 512;
-            if( toRead > length - i )
-                toRead = length - i;
-            memset(tmp, 0, toRead);
-            AK2I_CmdReadFlash(i, tmp, toRead);
-            read += toRead;
-            tmp += 512;
-            if (toRead < 512 || (read % 0x8000) == 0) {
-                DebugFileWrite(mem, read, offset);
-                offset += read;
-                read = 0;
-                tmp = mem;
-            }
+        if (!AK2I_CmdVerifyFlash(buffer + i, writeBegin + i, 512)) {
+            Debug("verify failed at %08X", i);
         }
-
-        FileClose();
-        ShowProgress(0, 0);
-
-        Debug("Done");
+        Debug("%08X", i);
     }
+
+    Debug("Lock flash");
+    AK2i_CmdLockFlash();
+
+    AK2I_CmdSetMapTableAddress(AK2I_MTN_NOR_OFFSET, 0);
+
+    //u32 acek_sign = 0x4B454341; // "ACEK"
+    //u32 ak2k_sign = 0x4B324B41; // "AK2K"
+
+    //ioAK2WriteFlash( NOR_FAT2_START, &ak2k_sign, 4 );
+
+    //end writting
+
+    uint8_t* mem = BUFFER_ADDRESS + 0x10000;
+    memset(mem, 0x41, 0x10000);
+
+    u32 length = hwVer == 0x81 ? 0x1000000 : 0x200000;
+    Debug("Dump patched flash... (length 0x%08X)", length);
+
+    char *filename = "ak2i_flash_patched.bin";
+    DebugFileCreate(filename, true);
+    uint8_t *tmp = mem;
+    for( u32 i = 0, offset = 0, read = 0; i < length; i += 512 ) {
+        ShowProgress(i, length);
+        u32 toRead = 512;
+        if( toRead > length - i )
+            toRead = length - i;
+        memset(tmp, 0, toRead);
+        AK2I_CmdReadFlash(i, tmp, toRead);
+        read += toRead;
+        tmp += 512;
+        if (toRead < 512 || (read % 0x8000) == 0) {
+            DebugFileWrite(mem, read, offset);
+            offset += read;
+            read = 0;
+            tmp = mem;
+        }
+    }
+
+    FileClose();
+    ShowProgress(0, 0);
+
+    Debug("Done");
 
     return 0;
 }
